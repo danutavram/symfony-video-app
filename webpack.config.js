2b3e18d24423912d606fb457e55fb73b5a2fb265
danutavram <avramnicolaedan@gmail.com>
2024-03-17 22:35:08 +0200

    Controllers, routesm views - install and move html template

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const Encore = require('@symfony/webpack-encore');
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/assets/')
+    .setPublicPath('/assets')
+    .addStyleEntry('css/dashboard', ['./assets/css/dashboard.css'])
+    .addStyleEntry('css/login', ['./assets/css/login.css'])
+    // .enableStimulusBridge('./assets/controllers.json').splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    // .enableBuildNotifications()
+
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/plugin-transform-class-properties');
+    // })
+
+    // .configureBabelPresetEnv((config) => {
+    //     config.useBuiltIns = 'usage';
+    //     config.corejs = 3;
+    // })
+;
+
+module.exports = Encore.getWebpackConfig();
